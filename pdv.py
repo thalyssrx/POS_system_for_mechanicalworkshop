85cd8432fe01dff43b80a0d6a790dee5643eedd6
Thalys <sopraver80@gmail.com>
2024-10-15 13:36:34 -0300

    fsc

---
+++ pdv.py
@@ -0,0 +1,55 @@
+from flet import *
+
+
+def main(page:Page):
+    page.title = "teste"
+    page.padding = 0
+    
+    a = Container(
+        margin=0,
+        padding=1,
+        alignment=alignment.center,
+        bgcolor=colors.GREY_600,
+        width=80,
+        height=40,
+        border_radius=0,
+        content= SubmenuButton(
+            content=Text('Produtos'),
+            controls=[
+                SubmenuButton(
+                    content=Text('a')
+                )
+                ]
+            )
+        )      
+    
+    menubar = MenuBar(
+        expand=True,
+        controls=[
+            SubmenuButton(
+                Text('Produtos'),
+                width=100,style=ButtonStyle(
+                    bgcolor=colors.GREY_500),
+                controls=[
+                    SubmenuButton(
+                        Text('Cadastro')
+                    )
+                ]
+            ),
+            SubmenuButton(
+                Text('cuuuu'),
+                width=100,style=ButtonStyle(
+                    bgcolor=colors.GREY_500),
+                controls=[
+                    SubmenuButton(
+                        Text('Cadastro')
+                    )
+                ]
+            )
+        ]
+    )
+    TopRow = Row([menubar])
+    
+    page.add(TopRow)
+
+app(main)
