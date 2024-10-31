2f4496095682f5e94d2de9e9a9137b5e874eecaf
Thalys <sopraver80@gmail.com>
2024-10-31 13:08:16 -0300

    MainPage Update/Login Icon

--- logscreen.py
+++ logscreen.py
@@ -10,22 +10,20 @@ def login(log:Page):
     log.window.always_on_top = True
     log.bgcolor=colors.WHITE
     log.padding = 0
-    
-    
-   
-    
+      
+    def click_close(e):
+        log.window.close()
+
     DragArea = WindowDragArea(
-        Container(
-        margin=0,padding=0),height=139,expand=True,maximizable=False)
+        Container(margin=0,padding=0),height=139,expand=True,maximizable=False)
         
     Colums_Text = WindowDragArea(
-        Container(
-        margin=0,padding=0),height=113, width=25,maximizable=False )  
+        Container(margin=0,padding=0),height=113,width=25,maximizable=False)  
     
     Text_Inputs=Column(
         alignment=MainAxisAlignment.CENTER,width=190,height=105,controls=([
-            TextField('',width=200,focused_border_color=colors.BLACK,fill_color=colors.WHITE,label='Nome',label_style=TextStyle(color=colors.BLACK)),
-            TextField('',width=200,focused_border_color=colors.BLACK,fill_color=colors.WHITE,label='Senha',label_style=TextStyle(color=colors.BLACK))
+            TextField('',width=200,focused_border_color=colors.BLACK,fill_color=colors.WHITE,label='Nome',label_style=TextStyle(color=colors.BLACK),cursor_color=colors.BLACK),
+            TextField('',width=200,focused_border_color=colors.BLACK,fill_color=colors.WHITE,label='Senha',label_style=TextStyle(color=colors.BLACK),cursor_color=colors.BLACK)
             ])
     )
     Row_Colums_Text = Row(spacing=0,controls=[
@@ -33,9 +31,13 @@ def login(log:Page):
             ])
     
     Full_View_Colum= Column(
-        spacing=0,controls=([DragArea,Row_Colums_Text,DragArea])
-    )
+        spacing=0,controls=([Stack([DragArea,IconButton(
+            icon=icons.CLOSE_ROUNDED,right=0,top=0,icon_color=colors.GREY,on_click=click_close
+        )]),Row_Colums_Text
+        ,Stack([DragArea,TextButton('Enter',right=19,top=10,style=ButtonStyle(
+            side=BorderSide(0.5,color='BLACK'),shape=RoundedRectangleBorder(radius=5),bgcolor=colors.INDIGO
+        ))])
+        ]))
 
     log.add(Full_View_Colum)
     
-app(login)
--- pdv.py
+++ pdv.py
@@ -2,17 +2,23 @@ from flet_multi_page import *
 import flet as ft
 from logscreen import login
 
-loginstart = subPage(target=login)
+
+loginwindow = subPage(target=login)
+
+
 def main(page:ft.Page):
-    page.title = "teste"
+    page.title = "PDV"
     page.padding = 0
     page.window.maximized= True
     page.window.resizable = False
+    page.bgcolor=ft.colors.WHITE
    
+    login_icon_menubar = ft.IconButton(
+        content= ft.Image(src="Icons/user.png",width=24,height=24,color=page.bgcolor),width=40,height=40,right=5)
+    
     menubar = ft.Row([ft.MenuBar(
-        
-        style=ft.MenuStyle(
-            padding=1,alignment=ft.alignment.top_right),
+        expand=True,style=ft.MenuStyle(
+            padding=0,alignment=ft.alignment.top_right,shape=ft.RoundedRectangleBorder(radius=0)),
         controls=[
             ft.SubmenuButton(
                 ft.Text('Produtos'),
@@ -29,20 +35,17 @@ def main(page:ft.Page):
                         ft.Text('Cadastro')
                     )
                 ]
-            ),
-            ft.IconButton(
-                icon=ft.icons.ABC, height= 40
             )
-        ]
-    )
-    ],width=ft.Window.width,height=40)
-
-
-
-    page.add(menubar)
-    page.update()
+        ])]
+        ,height=40,expand=True)
+    
+    
+    
+    page.add(ft.Stack([menubar,login_icon_menubar]))
+    
+    
 
 
 if __name__ == "__main__":
-    loginstart.start()
-    ft.app(target=main)
+    
+    ft.app(target=main,assets_dir='Assets')
